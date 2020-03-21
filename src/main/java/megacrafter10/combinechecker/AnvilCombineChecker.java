01cc00a12cdd619e5c9e6089e23ea03c268b350d
Mega <craftermega10@gmail.com>
2020-03-22 00:39:00 +0100

    using PrepareAnvilEvent instead of InventoryClickEvent
    result item is set to air if the enchants are not compatible

--- src/main/java/megacrafter10/combinechecker/AnvilCombineChecker.java
+++ src/main/java/megacrafter10/combinechecker/AnvilCombineChecker.java
@@ -1,13 +1,12 @@
 package megacrafter10.combinechecker;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -29,6 +28,30 @@ public final class AnvilCombineChecker extends JavaPlugin implements Listener {
         logger.info(getDescription().getName() + " disabled");
     }
 
+    @EventHandler
+    public void onEnchantmentCombine(PrepareAnvilEvent event) {
+        if (event.getInventory().getType() == InventoryType.ANVIL) {
+            ItemStack item1 = event.getInventory().getItem(0);
+            ItemStack item2 = event.getInventory().getItem(1);
+            if (item1 == null || item2 == null) {
+                return;
+            }
+            if (item1.getType() == Material.ENCHANTED_BOOK && item2.getType() == Material.ENCHANTED_BOOK) {
+                EnchantmentStorageMeta enchMeta1 = (EnchantmentStorageMeta) item1.getItemMeta();
+                EnchantmentStorageMeta enchMeta2 = (EnchantmentStorageMeta) item2.getItemMeta();
+                for (Enchantment ench : enchMeta1.getStoredEnchants().keySet()) {
+                    for (Enchantment ench2 : enchMeta2.getStoredEnchants().keySet()) {
+                        if (!EnchantmentData.isCompatible(ench.getName(), ench2.getName())) {
+                            event.setResult(new ItemStack(Material.AIR));
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /* this handler displays the result and displays an error message
     @EventHandler
     public void onEnchantmentCombine(InventoryClickEvent event){
         if (event.getInventory().getType() == InventoryType.ANVIL){
@@ -54,5 +77,5 @@ public final class AnvilCombineChecker extends JavaPlugin implements Listener {
                 }
             }
         }
-    }
+    }*/
 }
